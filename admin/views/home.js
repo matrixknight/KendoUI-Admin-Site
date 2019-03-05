6113b0d097b67e1e25631ad8d6e95cf01066f74f
IKKI2000 <ikki2002@qq.com>
2019-03-05 10:13:11 +0800

    首页更新区域图模块~

--- admin/views/home.js
+++ admin/views/home.js
@@ -13,6 +13,56 @@ $(function() {
             new CountUp('visitorCount', 0, res.count.visitor_count).start();
             // IP 数
             new CountUp('ipCount', 0, res.count.ip_count).start();
+            // 访客趋势图
+            $('#trendMonth').kendoChart({
+                theme: 'sass',
+                dataSource: {
+                    data: res.area,
+                    schema: {
+                        model: {
+                            id: 'uid',
+                            fields: {
+                                category: { type: 'string' },
+                                pv_count: { type: 'number' },
+                                visitor_count: { type: 'number' }
+                            }
+                        }
+                    }
+                },
+                legend: {
+                    position: 'top'
+                },
+                seriesDefaults: {
+                    type: 'area',
+                    area: {
+                        line: {
+                            width: 1,
+                            style: 'smooth'
+                        }
+                    },
+                    markers: {
+                        visible: true,
+                        size: 3
+                    }
+                },
+                series: [
+                    {
+                        field: 'pv_count',
+                        name: '浏览量（PV）'
+                    },
+                    {
+                        field: 'visitor_count',
+                        name: '访客数（UV）'
+                    }
+                ],
+                categoryAxis: {
+                    field: 'category'
+                },
+                tooltip: {
+                    visible: true,
+                    template: '#= value #'
+                }
+            });
         }
     });
 });
