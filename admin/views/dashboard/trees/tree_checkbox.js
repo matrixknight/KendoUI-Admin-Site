dc7c3afc2f4c303ad2be229686c1e6d3b0f53827
IKKI2000 <ikki2002@qq.com>
2019-03-26 15:52:36 +0800

    树形新增复选框及半选提交~

---
+++ admin/views/dashboard/trees/tree_checkbox.js
@@ -0,0 +1,144 @@
+var treeView;
+$(function() {
+    // 获取数据源生成树形
+    treeView = $('#treeView').kendoTreeView({
+        dataSource: {
+            transport: {
+                read: function(options) { readTree(options, 'json/tree.json') }
+            },
+            schema: {
+                data: 'data',
+                model: {
+                    id: 'id',
+                    children: 'items'
+                }
+            }
+        },
+        checkboxes: {
+            checkChildren: true
+        },
+        check: function(e) {
+            isSelectAll();
+            treeView.expand(e.node);
+            treeView.expand($(e.node).find('.k-item'));
+        },
+        select: function(e) {
+            if ($(e.node).find('a.k-link').length === 0) {
+                $('#treeDetail').empty();
+            }
+        },
+        dataBound: function() {
+            isSelectAll();
+            if ($('#treeView a.k-state-selected').length === 1) {
+                location.href = $('#treeView a.k-state-selected').attr('href');
+            }
+        }
+    }).data('kendoTreeView');
+    // 全选
+    $('#selectAll').click(function() {
+        if ($(this).prop('checked')) {
+            treeView.expand($('#treeView .k-item'));
+            $('#selectAll, #treeView :checkbox').prop('indeterminate', false).prop('checked', true);
+        } else {
+            treeView.expand($('#treeView .k-item'));
+            $('#selectAll, #treeView :checkbox').prop('indeterminate', false).prop('checked', false);
+        }
+    });
+});
+
+// 全选判断
+function isSelectAll() {
+    var ckb = $('#treeView > ul > li > div').find(':checkbox'),
+        ckbChecked = $('#treeView > ul > li > div').find(':checked');
+    if (ckb.length === ckbChecked.length) {
+        $('#selectAll').prop('indeterminate', false).prop('checked', true);
+    } else {
+        $.each(ckb, function() {
+            if ($(this).prop('checked') || $(this).prop('indeterminate')) {
+                $('#selectAll').prop('checked', false).prop('indeterminate', true);
+                return false;
+            } else {
+                $('#selectAll').prop('indeterminate', false).prop('checked', false);
+            }
+        });
+    }
+}
+
+// 提交 ID
+function submitIdTree() {
+    var ids = [];
+    $.each($('#treeView :checkbox'), function() {
+        if ($(this).prop('checked') || $(this).prop('indeterminate')) {
+            ids.push(treeView.dataItem($(this).closest('li')).id);
+        }
+    });
+    if (ids.length > 0) {
+        $('#loading').show();
+        $.fn.ajaxPost({
+            ajaxData: {
+                'ids': ids
+            },
+            finished: function() {
+                $('#loading').hide();
+            },
+            succeed: function() {
+                refreshTree();
+            },
+            isMsg: true
+        });
+    } else {
+        alertMsg('请先选择对象！', 'warning');
+    }
+}
+
+// 提交数据
+function submitDataTree() {
+    var models = [];
+    $.each($('#treeView :checkbox'), function() {
+        if ($(this).prop('checked') || $(this).prop('indeterminate')) {
+            models.push(treeView.dataItem($(this).closest('li')));
+        }
+    });
+    if (models.length > 0) {
+        $('#loading').show();
+        $.fn.ajaxPost({
+            ajaxData: {
+                'models': models
+            },
+            finished: function() {
+                $('#loading').hide();
+            },
+            succeed: function() {
+                refreshTree();
+            },
+            isMsg: true
+        });
+    } else {
+        alertMsg('请先选择对象！', 'warning');
+    }
+}
+
+// 显示详情
+function viewDetails(id) {
+    new kendo.data.DataSource({
+        transport: {
+            read: function(options) {
+                $.fn.ajaxPost({
+                    ajaxUrl: 'json/grid.json',
+                    succeed: function(res) {
+                        options.success(res);
+                    },
+                    failed: function(res) {
+                        options.error(res);
+                    }
+                });
+            }
+        },
+        schema: {
+            data: 'data'
+        },
+        change: function() {
+            $('#treeDetail').html(kendo.template($('#detailsTemplate').html())(this.get(id)));
+        }
+    }).read();
+}
