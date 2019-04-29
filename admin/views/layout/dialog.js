2a38230776fa44ccf01d241938fb7f1bc222e823
IKKI2000 <ikki2002@qq.com>
2019-04-29 10:05:44 +0800

    模态框和对话框显示及文字优化~

--- admin/views/layout/dialog.js
+++ admin/views/layout/dialog.js
@@ -18,27 +18,27 @@ $(function () {
     // 确认框
     $('#confirmMsgSuccess').click(function () {
         confirmMsg('成功信息', '这里是成功信息的内容~', 'success', function () {
-            alertMsgNoBtn('你点了确定~', 'info');
+            alertMsgNoBtn('你选择了确定~', 'info');
         });
     });
     $('#confirmMsgInfo').click(function () {
         confirmMsg('普通信息', '这里是普通信息的内容~', 'info', function () {
-            alertMsgNoBtn('你点了确定~', 'info');
+            alertMsgNoBtn('你选择了确定~', 'info');
         });
     });
     $('#confirmMsgQuestion').click(function () {
         confirmMsg('提问信息', '这里是提问信息的内容~', 'question', function () {
-            alertMsgNoBtn('你点了确定~', 'info');
+            alertMsgNoBtn('你选择了确定~', 'info');
         });
     });
     $('#confirmMsgWarning').click(function () {
         confirmMsg('警告信息', '这里是警告信息的内容~', 'warning', function () {
-            alertMsgNoBtn('你点了确定~', 'info');
+            alertMsgNoBtn('你选择了确定~', 'info');
         });
     });
     $('#confirmMsgError').click(function () {
         confirmMsg('错误信息', '这里是错误信息的内容~', 'error', function () {
-            alertMsgNoBtn('你点了确定~', 'info');
+            alertMsgNoBtn('你选择了确定~', 'info');
         });
     });
     // 对话框无按钮
@@ -54,25 +54,25 @@ $(function () {
             minWidth: 320,
             minHeight: 196,
             title: '标题',
-            content: '自定义按钮对话框~',
+            content: '这里是自定义按钮对话框~',
             actions: [
                 {
                     text: '安装更新',
                     primary: true,
                     action: function () {
-                        alertMsgNoBtn('你点了安装更新~', 'info');
+                        alertMsgNoBtn('你选择了安装更新~', 'info');
                     }
                 },
                 {
                     text: '忽略更新',
                     action: function () {
-                        alertMsgNoBtn('你点了忽略更新~', 'info');
+                        alertMsgNoBtn('你选择了忽略更新~', 'info');
                     }
                 },
                 {
                     text: '下次提醒我',
                     action: function () {
-                        alertMsgNoBtn('你点了下次提醒我~', 'info');
+                        alertMsgNoBtn('你选择了下次提醒我~', 'info');
                     }
                 }
             ],
@@ -88,18 +88,18 @@ $(function () {
     });
     // Kendo UI 原生确认框
     $('#confirmBtn').click(function () {
-        kendo.confirm('这里是 Kendo UI 原生确认框的信息？').then(function () {
-            kendo.alert('你点了确定~');
+        kendo.confirm('这里是 Kendo UI 原生确认框的信息~').then(function () {
+            kendo.alert('你选择了确定~');
         }, function () {
-            kendo.alert('你点了取消~');
+            kendo.alert('你选择了取消~');
         });
     });
     // Kendo UI 原生提示框
     $('#promptBtn').click(function () {
-        kendo.prompt('这里是 Kendo UI 原生提示框的信息？', '填写内容').then(function (data) {
+        kendo.prompt('这里是 Kendo UI 原生提示框的信息~', '默认内容').then(function (data) {
             kendo.alert(kendo.format('你填写的是："{0}"', data));
         }, function () {
-            kendo.alert('你点了取消~');
+            kendo.alert('你选择了取消~');
         });
     });
 });
--- admin/views/layout/window.js
+++ admin/views/layout/window.js
@@ -1,44 +1,44 @@
 $(function () {
     // 警告框小按钮
     $('#alertMsgSuccess').click(function () {
-        alertMsgBtn('这里是成功信息的内容~', 'success');
+        alertMsgBtn('这里是成功窗口的内容~', 'success');
     });
     $('#alertMsgInfo').click(function () {
-        alertMsgBtn('这里是普通信息的内容~', 'info');
+        alertMsgBtn('这里是普通窗口的内容~', 'info');
     });
     $('#alertMsgQuestion').click(function () {
-        alertMsgBtn('这里是提问信息的内容~', 'question');
+        alertMsgBtn('这里是提问窗口的内容~', 'question');
     });
     $('#alertMsgWarning').click(function () {
-        alertMsgBtn('这里是警告信息的内容~', 'warning');
+        alertMsgBtn('这里是警告窗口的内容~', 'warning');
     });
     $('#alertMsgError').click(function () {
-        alertMsgBtn('这里是错误信息的内容~', 'error');
+        alertMsgBtn('这里是错误窗口的内容~', 'error');
     });
     // 确认框小按钮
     $('#confirmMsgSuccess').click(function () {
-        confirmMsgBtn('成功信息', '这里是成功信息的内容~', 'success', function () {
-            alertMsgNoBtn('你点了确定~', 'info');
+        confirmMsgBtn('成功窗口', '这里是成功窗口的内容~', 'success', function () {
+            alertMsgNoBtn('你选择了确定~', 'info');
         });
     });
     $('#confirmMsgInfo').click(function () {
-        confirmMsgBtn('普通信息', '这里是普通信息的内容~', 'info', function () {
-            alertMsgNoBtn('你点了确定~', 'info');
+        confirmMsgBtn('普通窗口', '这里是普通窗口的内容~', 'info', function () {
+            alertMsgNoBtn('你选择了确定~', 'info');
         });
     });
     $('#confirmMsgQuestion').click(function () {
-        confirmMsgBtn('提问信息', '这里是提问信息的内容~', 'question', function () {
-            alertMsgNoBtn('你点了确定~', 'info');
+        confirmMsgBtn('提问窗口', '这里是提问窗口的内容~', 'question', function () {
+            alertMsgNoBtn('你选择了确定~', 'info');
         });
     });
     $('#confirmMsgWarning').click(function () {
-        confirmMsgBtn('警告信息', '这里是警告信息的内容~', 'warning', function () {
-            alertMsgNoBtn('你点了确定~', 'info');
+        confirmMsgBtn('警告窗口', '这里是警告窗口的内容~', 'warning', function () {
+            alertMsgNoBtn('你选择了确定~', 'info');
         });
     });
     $('#confirmMsgError').click(function () {
-        confirmMsgBtn('错误信息', '这里是错误信息的内容~', 'error', function () {
-            alertMsgNoBtn('你点了确定~', 'info');
+        confirmMsgBtn('错误窗口', '这里是错误窗口的内容~', 'error', function () {
+            alertMsgNoBtn('你选择了确定~', 'info');
         });
     });
     // 弹出层
@@ -48,24 +48,6 @@ $(function () {
     // 弹出页
     $('#iframeWindow').click(function () {
         iframeWindow('弹出页', '80%', '40%', 'admin/login.html');
-    });
-    // 大图预览
-    $('#showBigPic').click(function () {
-        showBigPic('img/lock_bg.jpg');
-    });
-    // 限制范围
-    $('#rangeWindow').click(function () {
-        var divWindow = $('<div class="window-box"></div>').kendoWindow({
-            draggable: {
-                containment: "#container"
-            },
-            title: '限制层',
-            modal: true,
-            close: function () {
-                divWindow.destroy();
-            }
-        }).data('kendoWindow');
-        divWindow.content('<div>拖放窗口无法超出阴影限制的范围~</div>').open();
     });
     // 自定义模态框
     $('#customWindow').click(function () {
@@ -85,8 +67,26 @@ $(function () {
         }).data('kendoWindow');
         divWindow.content('<div class="py-3">左上角标题图标及右上角第一个按钮均为自定义~</div>').center().open();
         divWindow.wrapper.find('.k-i-custom').parent().click(function (e) {
-            divWindow.title('换个标题');
+            divWindow.title('标题已更换');
             e.preventDefault();
         });
+    });
+    // 大图预览
+    $('#showBigPic').click(function () {
+        showBigPic('img/lock_bg.jpg');
+    });
+    // 限制范围
+    $('#rangeWindow').click(function () {
+        var divWindow = $('<div class="window-box"></div>').kendoWindow({
+            draggable: {
+                containment: "#container"
+            },
+            title: '限制层',
+            modal: true,
+            close: function () {
+                divWindow.destroy();
+            }
+        }).data('kendoWindow');
+        divWindow.content('<div>拖放窗口无法超出阴影限制的范围~</div>').open();
     });
 });
